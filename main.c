a5e157f0da2cd70df121c5fd93bb478342b58d39
Aayush Chaudhary <acaayush1111@gmail.com>
2022-02-04 11:33:01 +0530

    Update main

--- main.c
+++ main.c
@@ -1,38 +1,86 @@
 #include <stdio.h>
-//#include <conio.h>
+#include "conio.h"
+#include <stdlib.h>
 #define max 20
 struct empDat {
-    char * name; 
-    long int code; 
-    char * designation;
-    int exp;
-    long int pay; 
+    long int eNo; 
+    char * eName; 
+    long int salary; 
 };
 int num; 
 
-void showMenu(); 
-
-struct empDat emp[max];
-struct empDat tempemp[max];
-struct empDat sortemp[max];
-struct empDat sortemp1[max];
-
-void build()
+void init()
 {
-    printf("Buildi the data table. \n");
-    printf("Maximum entries can be %d \n", max); 
-
-    printf("Enter the number of entries required: \n"); 
-    scanf("%d", num);
-
-    printf("So you want %d of them? \n ", num); 
-
+    printf("Hello, Weclome! \n \n");
+    printf("Employee Data Handler \n");
+    printf("by Aniruddha Maradwar \n");
+    printf("\n \n");
 }
-int main(){
 
-    printf("hallo, world! \n"); 
-    build(); 
+int menu()
+{
+    int menuOption; 
 
-   // printf("wait %c", getch()); 
+    printf("Options: \n \n "); 
+    printf("1. Enter Data \n ");
+    printf("2. Display Employee Data \n "); 
+    printf("3. Search Employee by Name \n "); 
+    printf("4. Search Employee by Employee Number \n "); 
+    printf("5. Display all employees having salary between 20000 to 5000 \n "); 
+    printf("6. Quit.");
+    printf("\n\n"); 
+    printf("Choose Option:- "); scanf("%d", &menuOption); 
+    return menuOption;  
+}
+
+void perform(int actionToPerform)
+{
+    printf("You chose: %d \n", actionToPerform);
+    switch (actionToPerform)
+    {
+        // clear screen 
+        system(clear); 
+        case 1: 
+        printf("Enter Data:- \n"); 
+        break; 
+
+        case 2: 
+        printf("Display Employee Data"); 
+        break;
+
+        case 3:
+        break; 
+
+        case 4:
+        break; 
+
+        case 5:
+        break; 
+
+        case 6:
+        //quit program
+        break; 
+
+        default: 
+        //exit to main menu
+        printf("Incorrect Option \n"); 
+        //clear screen
+        menu(); 
+        break; 
+    }
+}
+
+
+int main()
+{
+    int action = 0; 
+    init(); 
+    action = menu();
+     
+    //clear screen 
+    perform(action); 
+
+    //system("clear"); 
+    printf("wait, %c", getch()); 
     return 0;
 }
